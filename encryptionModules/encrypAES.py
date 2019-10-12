53430408462432e5fb085a88c8d6a8244aa3f772
Sakshat Shinde <sakshatshinde@gmail.com>
2019-10-12 16:38:05 +0530

    Added initial keyStore encryption
    Encryting the keyStore file so no one can manually access the file. Also deletes the files ones used. As it is meant to be used only once when its deployed.

---
+++ encryptionModules/encrypAES.py
@@ -0,0 +1,41 @@
+import pyAesCrypt
+import os
+
+# encryption/decryption buffer size - 64K
+bufferSize = 64 * 1024
+
+try:
+    fileRef = open("keys.txt", "r")
+    keyList = fileRef.readlines()
+    key = keyList[0]
+    fileRef.close()
+
+except:
+    #Checking if it is already been executed
+    print("Already nuked")
+    quit()
+
+# encryption
+def aesEncrypt():
+    encryptThis = "keys.txt"     #target
+    global encryptDone 
+    encryptDone = str(encryptThis + ".sss")
+    pyAesCrypt.encryptFile(encryptThis, encryptDone, key, bufferSize)
+
+
+def destroyKey():
+    #removing the plain text key file immediately after usage
+    if os.path.exists("keys.txt"):
+        os.remove("keys.txt")
+    else:
+        pass
+
+
+# decryption
+def aesDecrypt():
+    decryptThis = encryptDone
+    pyAesCrypt.decryptFile(decryptThis, "recover.txt", key, bufferSize)
+
+aesEncrypt()
+destroyKey()
+aesDecrypt()
