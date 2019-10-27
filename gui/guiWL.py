53afd5fb37490521d6b1e595f6ab394c622646f9
Sakshat Shinde <sakshatshinde@gmail.com>
2019-10-27 15:12:54 +0530

    Removed window interactions
    User cannot close,resize or move the window. Now it does not appear in the task bar either. By default it opens in the center of the screen.

--- gui/guiWL.py
+++ gui/guiWL.py
@@ -1,14 +1,20 @@
-#import tkinter as tk
+
 from tkinter import *
 import os, winsound
 from tkinter.ttk import *
 
 bgVal = "#212121"
-fgVal = "#ffeb3b"
+fgVal = "#fbc02d"
 
 root = Tk()
 root.configure(background=bgVal, height = 700, width = 820)
-root.resizable(FALSE, FALSE)
+root.resizable(FALSE, FALSE)    #resizing turned off
+
+#sets the window in center of the screen
+root.eval('tk::PlaceWindow %s center' % root.winfo_pathname(root.winfo_id()))
+
+#removes title bar and removes the ability to close the application from task bar
+root.overrideredirect(True)
 
 art = """
 
@@ -22,8 +28,6 @@ art = """
 """
 
 Label(root,text=art, font='TkFixedFont', background=bgVal, foreground=fgVal).place(x = 5,y = 0)
-
-
 
 #playing music
 winsound.PlaySound('extras/Goosebumps Theme Song (Caspro Remix).wav', winsound.SND_ALIAS | winsound.SND_LOOP + winsound.SND_ASYNC)
