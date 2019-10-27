60266aa4e996294489b568f8fca6426ddf80dde4
Sakshat Shinde <sakshatshinde@gmail.com>
2019-10-27 14:20:34 +0530

    Update guiWL.py

--- gui/guiWL.py
+++ gui/guiWL.py
@@ -7,6 +7,7 @@ from tkinter.ttk import *
 root = Tk()
 root.resizable(False, False)
 
+
 canvas = Canvas(root, height = 700, width = 820, bg = "#212121")
 
 art = """
@@ -21,10 +22,10 @@ art = """
 """
 
 style = Style()
-style.configure('Style.TButton', font='TkFixedFont', foreground="#dd2c00", bg="#212121")
-label = Label(root,text=art, style='Style.TButton').place(x = 0,y = 0)
-
+style.configure('Style.TButton', font='TkFixedFont', foreground="#dd2c00")
+lab = Label(root,text=art, style='Style.TButton').place(x = 0,y = 0)
 canvas.pack()
+
 
 #playing music
 winsound.PlaySound('extras/Goosebumps Theme Song (Caspro Remix).wav', winsound.SND_ALIAS | winsound.SND_LOOP + winsound.SND_ASYNC)
