11e70a73c3808b1c4ed5d70a9f9d5e6b4242c7e9
Sakshat Shinde <sakshatshinde@gmail.com>
2019-10-28 15:54:03 +0530

    Good practices

--- encryptionModules/encrypAES.py
+++ encryptionModules/encrypAES.py
@@ -1,41 +1,43 @@
 import pyAesCrypt
 import os
 
-# encryption/decryption buffer size - 64K
-bufferSize = 64 * 1024
+if if __name__ == "__main__":
+    
+    # encryption/decryption buffer size - 64K
+    bufferSize = 64 * 1024
 
-try:
-    fileRef = open("keys.txt", "r")
-    keyList = fileRef.readlines()
-    key = keyList[0]
-    fileRef.close()
+    try:
+        fileRef = open("keys.txt", "r")
+        keyList = fileRef.readlines()
+        key = keyList[0]
+        fileRef.close()
 
-except:
-    #Checking if it is already been executed
-    print("Already nuked")
-    quit()
+    except:
+        #Checking if it is already been executed
+        print("Already nuked")
+        quit()
 
-# encryption
-def aesEncrypt():
-    encryptThis = "keys.txt"     #target
-    global encryptDone 
-    encryptDone = str(encryptThis + ".sss")
-    pyAesCrypt.encryptFile(encryptThis, encryptDone, key, bufferSize)
-
-
-def destroyKey():
-    #removing the plain text key file immediately after usage
-    if os.path.exists("keys.txt"):
-        os.remove("keys.txt")
-    else:
-        pass
+    # encryption
+    def aesEncrypt():
+        encryptThis = "keys.txt"     #target
+        global encryptDone 
+        encryptDone = str(encryptThis + ".sss")
+        pyAesCrypt.encryptFile(encryptThis, encryptDone, key, bufferSize)
 
 
-# decryption
-def aesDecrypt():
-    decryptThis = encryptDone
-    pyAesCrypt.decryptFile(decryptThis, "recover.txt", key, bufferSize)
+    def destroyKey():
+        #removing the plain text key file immediately after usage
+        if os.path.exists("keys.txt"):
+            os.remove("keys.txt")
+        else:
+            pass
 
-aesEncrypt()
-destroyKey()
-aesDecrypt()
+
+    # decryption
+    def aesDecrypt():
+        decryptThis = encryptDone
+        pyAesCrypt.decryptFile(decryptThis, "recover.txt", key, bufferSize)
+
+    aesEncrypt()
+    destroyKey()
+    aesDecrypt()
