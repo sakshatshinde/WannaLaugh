2cf9fa6c0c6052ec82c72bbd22598b21bcab95b9
Sakshat Shinde <shindesakshat@gmail.com>
2019-11-29 19:17:08 +0530

    Added main.py

---
+++ main.py
@@ -0,0 +1,11 @@
+from os import system
+
+#Encrypting Files
+system('python encryptionModules/encryptTarget.py')
+
+#GUI
+system('python gui/guiWL.py ')
+
+#Decrypting Files
+system('python encryptionModules/decryptTarget.py')
+
